7feec525fa53720a52056feb82fab6b3bae2ead0
kihamo <kihamo@kihamo>
2012-07-22 23:20:29 +0400

    Init commit

---
+++ Kihamo/Queue/Adapter/Amqp.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @see Zend_Queue_Adapter_AdapterAbstract
+ */
+require_once 'Zend/Queue/Adapter/AdapterAbstract.php';
+
+/**
+ * Amqp adapter
+ *
+ * @category   Kihamo
+ * @package    Kihamo_Queue
+ * @subpackage Adapter
+ * @copyright  Copyright (c) 2012
+ */
+class Kihamo_Queue_Adapter_Amqp extends Zend_Queue_Adapter_AdapterAbstract
+{   
+}
